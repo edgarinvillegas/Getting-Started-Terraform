f004f1c4843ead27a10dad1c70246bb5c4202964
Edgar Villegas <edgarinvillegas@hotmail.com>
2022-01-31 18:40:45 -0400

    feat: Parameterized aws variables

---
+++ globo_webapp/variables.tf
@@ -0,0 +1,28 @@
+variable aws_access_key {
+  type = string
+  description = "AWS access key"
+  sensitive = true
+}
+
+variable aws_secret_key {
+  type = string
+  description = "AWS secret key"
+  sensitive = true
+}
+
+variable aws_region {
+  type = string
+  default = "us-west-1"
+}
+
+variable vpc_cidr { default = "10.0.0.0/16" }
+
+variable subnet_cidr { default = "10.0.0.0/24" }
+
+variable http_port {
+  type = number
+  description = "Port for the nginx web server"
+  default = 80
+}
+
+variable webserver_instance_type { default = "t2.micro" }
