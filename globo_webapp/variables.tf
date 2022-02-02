924dbd7ff85c083c9504d779f3fd95f7b42155bd
Edgar Villegas <edgarinvillegas@hotmail.com>
2022-02-02 16:52:05 -0400

    feat: Create network.tf, outputs.tf, variables.tf, instances.tf and loadbalancer.tf

--- globo_webapp/variables.tf
+++ globo_webapp/variables.tf
@@ -17,7 +17,7 @@ variable aws_region {
 
 variable vpc_cidr { default = "10.0.0.0/16" }
 
-variable subnet_cidr { default = "10.0.0.0/24" }
+variable subnet_cidr { default = ["10.0.1.0/24", "10.0.2.0/24"] }
 
 variable http_port {
   type = number
